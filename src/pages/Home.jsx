48b115949024eb7bdc20b6f71c95caffd5844096
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 01:02:59 +0200

    Add Assets folder with files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,11 @@ const Home = () => {
         <section className="w-full h-screen relative">
             <Canvas className='w-full h-screen bg-transparent' camera={{near: 0.1, far: 1000}}>
                 <Suspense fallback={<Loader/>}>
-
+                    <directionalLight/>
+                    <ambientLight/>
+                    <pointLight/>
+                    <spotLight/>
+                    <hemisphereLight/>
                 </Suspense>
             </Canvas>
         </section>
