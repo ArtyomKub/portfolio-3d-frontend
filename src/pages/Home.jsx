eaa314b2cb3e01021e94e09324625dc507de3aeb
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 00:31:52 +0200

    Create a structure for Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+
+{/*<div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>*/}
+{/*    POPUP*/}
+{/*</div>*/}
+
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="w-full h-screen relative">
+
+
+
+        </section>
     );
 };
 
