3a59a6a36e94343d91e283855c94268415effbcb
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 00:36:42 +0200

    Use a Canvas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,9 @@ import {Canvas} from "@react-three/fiber";
 const Home = () => {
     return (
         <section className="w-full h-screen relative">
+            <Canvas className='w-full h-screen bg-transparent' camera={{near:0.1, far: 1000}}>
 
-
-
+            </Canvas>
         </section>
     );
 };
