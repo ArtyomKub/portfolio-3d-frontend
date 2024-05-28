c8516999d2c359f13e6b6d3d79c5bfd20343ca7d
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 15:05:36 +0200

    Connect new component Sky in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import {Suspense} from "react";
 import {Canvas} from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island"
+import Sky from "../models/Sky.jsx";
 
 {/*<div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>*/
 }
@@ -34,6 +35,9 @@ const Home = () => {
                     <directionalLight position={[1,1,1]} intensity={2}/>
                     <ambientLight intensity={0.5}/>
                     <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1}/>
+
+                    <Sky/>
+
                     <Island position={islandPosition}
                             scale={islandScale}
                             rotation={islandRotation}
