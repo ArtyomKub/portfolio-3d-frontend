caabe034742055180d71a3313a0f42355c503cf9
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 15:16:07 +0200

    Create a bird scene

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import birdScene from '../assets/3d/bird.glb'
+import {useGLTF} from "@react-three/drei";
+
 
 const Bird = () => {
+    const bird = useGLTF(birdScene)
     return (
         <mesh>
-
+            <primitive object={bird.scene}/>
         </mesh>
     );
 };
