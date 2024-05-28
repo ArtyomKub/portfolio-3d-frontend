7b0a4612b248f35f05e18ee1717cecd84a45ed57
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 22:03:47 +0200

    Create index.js with specializations

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const About = () => {
+export const About = () => {
     return (
-        <div>
-ABOUT
-        </div>
-    );
-};
+        <section className='max-container'>
+            <h1 className='head-text'>
+                Hello, I'm{" "}
+                <span className='blue-gradient_text font-semibold drop-shadow'>{" "}Artsem</span>{" "}👋
+            </h1>
+            <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+                <p>
+                    Front-end Developer based in Poland (Poznan), specializing in building applications.
+                </p>
+            </div>
+            <div className='py-10 flex flex-col'>
+                <h3 className='subhead-text'>My Skills</h3>
+                <div className='mt-16 flex flex-wrap gap-12'>
 
-export default About;
+
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Home from "./Home.jsx";
-import About from "./About.jsx";
 import Projects from "./Projects.jsx";
 import Contact from "./Contact.jsx";
+import {About} from "./About.jsx";
 
 export {Home, About, Projects, Contact};
