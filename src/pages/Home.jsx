16629a5b37b5459779cdf3e3a3a709335d30a236
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 20:41:25 +0200

    Fix error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,37 @@
 import {Canvas} from "@react-three/fiber";
 import {Suspense, useState} from "react";
+
 import HomeInfo from "../components/HomeInfo.jsx";
 import Loader from "../components/Loader.jsx";
-import Bird from '../models/Bird.jsx'
+import Bird from "../models/Bird.jsx";
 import Sky from "../models/Sky.jsx";
 import Island from "../models/Island.jsx";
 import Plane from "../models/Plane.jsx";
 
-
 const Home = () => {
 
-    const [currentStage, setCurrentStage] = useState(1)
+    const [currentStage, setCurrentStage] = useState(1);
+    const [isRotating, setIsRotating] = useState(false);
 
-    const [isRotating, setIsRotating] = useState(false)
 
-    const adjustPlaneForScreenSize = () => {
+    const adjustBiplaneForScreenSize = () => {
         let screenScale, screenPosition;
+
+        // If screen width is less than 768px, adjust the scale and position
         if (window.innerWidth < 768) {
             screenScale = [1.5, 1.5, 1.5];
-            screenPosition = [0, -1.5, 0]
+            screenPosition = [0, -1.5, 0];
         } else {
             screenScale = [3, 3, 3];
-            screenPosition = [0, -4, -4]
+            screenPosition = [0, -4, -4];
         }
-        return [screenScale, screenPosition]
-    }
+
+        return [screenScale, screenPosition];
+    };
 
     const adjustIslandForScreenSize = () => {
-        let screenScale, screenPosition
+        let screenScale, screenPosition;
+
         if (window.innerWidth < 768) {
             screenScale = [0.9, 0.9, 0.9];
             screenPosition = [0, -6.5, -43.4];
@@ -39,18 +43,21 @@ const Home = () => {
         return [screenScale, screenPosition];
     };
 
-    const [planeScale, planePosition] = adjustPlaneForScreenSize()
-
+    const [biplaneScale, biplanePosition] = adjustBiplaneForScreenSize();
     const [islandScale, islandPosition] = adjustIslandForScreenSize();
 
-
     return (
-        <section className="w-full h-screen relative">
+        <section className='w-full h-screen relative'>
             <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
                 {currentStage && <HomeInfo currentStage={currentStage}/>}
             </div>
-            <Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
-                    camera={{near: 0.1, far: 1000}}>
+
+            <Canvas
+                className={`w-full h-screen bg-transparent ${
+                    isRotating ? "cursor-grabbing" : "cursor-grab"
+                }`}
+                camera={{near: 0.1, far: 1000}}
+            >
                 <Suspense fallback={<Loader/>}>
                     <directionalLight position={[1, 1, 1]} intensity={2}/>
                     <ambientLight intensity={0.5}/>
@@ -66,19 +73,22 @@ const Home = () => {
                         groundColor='#000000'
                         intensity={1}
                     />
-
                     <Bird/>
                     <Sky isRotating={isRotating}/>
-                    <Island position={islandPosition}
-                            scale={islandScale}
-                            rotation={[0.1, 4.7077, 0]}
-                            isRotaring={isRotating}
-                            setIsRotating={setIsRotating}
-                            setCurrentStage={setCurrentStage}
+                    <Island
+                        isRotating={isRotating}
+                        setIsRotating={setIsRotating}
+                        setCurrentStage={setCurrentStage}
+                        position={islandPosition}
+                        rotation={[0.1, 4.7077, 0]}
+                        scale={islandScale}
                     />
-                    <Plane isRotating={isRotating}
-                           planeScale={planeScale} planePosition={planePosition}
-                           rotation={[0, 20.1, 0]}/>
+                    <Plane
+                        isRotating={isRotating}
+                        position={biplanePosition}
+                        rotation={[0, 20.1, 0]}
+                        scale={biplaneScale}
+                    />
                 </Suspense>
             </Canvas>
         </section>
