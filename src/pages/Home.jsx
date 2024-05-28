079cc9632be42b9ce8e04035bf090cfddcb43e94
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 16:17:34 +0200

    Ad props in component Plane

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,6 @@ const Home = () => {
     }
     const adjustPlaneForScreenSize = () => {
         let screenScale, screenPosition;
-
         if (window.innerWidth < 768) {
             screenScale = [1.5, 1.5, 1.5];
             screenPosition = [0, -1.5, 0]
@@ -34,7 +33,7 @@ const Home = () => {
             screenScale = [3, 3, 3];
             screenPosition = [0, -4, -4]
         }
-        return [screenScale, screenPosition, rotation]
+        return [screenScale, screenPosition]
     }
 
     const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
