693d9d0efe9e6889a548a247aefd8da88c8b58eb
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 02:20:12 +0200

    Found error and check it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Suspense} from "react";
 import {Canvas} from "@react-three/fiber";
-import Loader from "../components/Loader.jsx";
+import Loader from "../components/Loader";
+import Island from "../models/Island"
 
 {/*<div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>*/
 }
@@ -11,6 +12,21 @@ import Loader from "../components/Loader.jsx";
 }
 
 const Home = () => {
+    const adjustIslandForScreenSize = () => {
+        let screenScale = null;
+        let screenPosition = [0, -6.5, -43];
+        let rotation = [0.1, 4.7, 0];
+
+        if (window.innerWidth < 768) {
+            screenScale = [0.9, 0.9, 0.9];
+        } else {
+            screenScale = [1, 1, 1];
+        }
+        return [screenScale, screenPosition, rotation]
+    }
+
+    const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
     return (
         <section className="w-full h-screen relative">
             <Canvas className='w-full h-screen bg-transparent' camera={{near: 0.1, far: 1000}}>
@@ -20,6 +36,10 @@ const Home = () => {
                     <pointLight/>
                     <spotLight/>
                     <hemisphereLight/>
+                    <Island position={islandPosition}
+                            scale={islandScale}
+                            rotation={islandRotation}
+                    />
                 </Suspense>
             </Canvas>
         </section>
