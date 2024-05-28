aeab2c5a65ea2ccdbf419bec6d08a9b051c6e105
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 14:52:55 +0200

    Add a directional Light

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
         <section className="w-full h-screen relative">
             <Canvas className='w-full h-screen bg-transparent' camera={{near: 0.1, far: 1000}}>
                 <Suspense fallback={<Loader/>}>
-                    <directionalLight />
+                    <directionalLight position={[1,1,1]} intensity={2}/>
                     <ambientLight/>
                     <pointLight/>
                     <spotLight/>
