777f5133ad55a3cdfba2680934128021a363a7fe
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 14:54:53 +0200

    Add ambient Light

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
             <Canvas className='w-full h-screen bg-transparent' camera={{near: 0.1, far: 1000}}>
                 <Suspense fallback={<Loader/>}>
                     <directionalLight position={[1,1,1]} intensity={2}/>
-                    <ambientLight/>
+                    <ambientLight intensity={0.5}/>
                     <pointLight/>
                     <spotLight/>
                     <hemisphereLight/>
