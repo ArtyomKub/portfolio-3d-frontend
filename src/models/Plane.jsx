8e124d9246dc0d6162943377ee130c8cd8179fc9
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 15:16:16 +0200

    Create a plane scene

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import planeScene from '../assets/3d/plane.glb'
+import {useGLTF} from "@react-three/drei";
 
 const Plane = () => {
+    const plane = useGLTF(planeScene)
     return (
         <mesh>
-
+            <primitive object={plane.scene}/>
         </mesh>
     );
 };
