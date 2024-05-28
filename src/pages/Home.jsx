a74f4241489c2060e64f592817e2c5423d3e2277
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 15:12:05 +0200

    Connect our components Bird and Plane in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,9 @@ import {Canvas} from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island"
 import Sky from "../models/Sky.jsx";
+import Bird from "../models/Bird.jsx";
+import Plane from "../models/Plane.jsx";
 
-{/*<div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>*/
-}
-{/*    POPUP*/
-}
-{/*</div>*/
-}
 
 const Home = () => {
     const adjustIslandForScreenSize = () => {
@@ -32,16 +28,16 @@ const Home = () => {
         <section className="w-full h-screen relative">
             <Canvas className='w-full h-screen bg-transparent' camera={{near: 0.1, far: 1000}}>
                 <Suspense fallback={<Loader/>}>
-                    <directionalLight position={[1,1,1]} intensity={2}/>
+                    <directionalLight position={[1, 1, 1]} intensity={2}/>
                     <ambientLight intensity={0.5}/>
                     <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1}/>
-
+                    <Bird/>
                     <Sky/>
-
                     <Island position={islandPosition}
                             scale={islandScale}
                             rotation={islandRotation}
                     />
+                    <Plane/>
                 </Suspense>
             </Canvas>
         </section>
