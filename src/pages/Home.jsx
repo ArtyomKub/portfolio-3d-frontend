45b516bd7cdb39f4dd7411c487b2d3dcb53e0599
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-28 02:21:25 +0200

    Found error and check it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
         <section className="w-full h-screen relative">
             <Canvas className='w-full h-screen bg-transparent' camera={{near: 0.1, far: 1000}}>
                 <Suspense fallback={<Loader/>}>
-                    <directionalLight/>
+                    <directionalLight />
                     <ambientLight/>
                     <pointLight/>
                     <spotLight/>
